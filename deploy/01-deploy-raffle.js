fd7d4475d2bcedebc61bec404722a4d708fee378
Chizitere Okey-Awuzie <chizzit45@gmail.com>
2023-07-28 15:36:41 +0100

    Done with the staging test

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,6 +1,6 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
-const { verify } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
 module.exports = async function ({ deployments, getNamedAccounts }) {
